3889c85a0d6541760c77583efe23d37b37420577
Максим Чернышев <maksimchernyshev@MacBook-Air-Maksim.local>
2020-10-05 14:18:34 +0300

    starting to make profession blocks with full description

---
+++ js/readMoreButton.js
@@ -0,0 +1,9 @@
+jQuery('.content-description').readmore({
+    maxHeight: 100,
+    speed: 70,
+    collapsedHeight: 196,
+    heightMargin: 16,
+    moreLink: '<a href="#" class="toggle-content-text">Подробнее</a>',
+    lessLink: '<a href="#" class="toggle-content-text">Свернуть</a>',
+    embedCSS: true
+});
